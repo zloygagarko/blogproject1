c366e912c3dd11dc1f32841ed6e69b1116df1e1f
zloygagarko <zloygagarko12@gmail.com>
2024-08-05 11:43:46 -0500

    first

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'cblog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'database1', # database name in RDS is written here
+        'NAME': 'project_db', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
         'PASSWORD': 'admin12345',
-        'HOST': 'database-1.chskeku2ysv9.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'HOST': 'terraform-20240805163034768900000001.chswaeeaatrr.us-east-2.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'blog-mediafiles' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'zloygagarko' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
